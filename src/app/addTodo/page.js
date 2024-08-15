1ff3090e23dca7027b335ce3516122bbb618adc0
redmi <hmadaredmi7@gmail.com>
2024-08-15 15:33:58 +0300

    ahmed3

--- src/app/addTodo/page.js
+++ src/app/addTodo/page.js
@@ -35,23 +35,20 @@ export default function Page() {
     
 
   return (
-    <div className="bg-black h-full w-full m-0 py-10">
+    <div className=" h-full w-full m-0 py-10">
         <div className="w-full flex justify-center ">
-            <input ref={ref0} type="number" placeholder="ادخل رقم الاسبوع..." className="p-3 appearance-none bg-black text-white font-bold rounded-md"/>
+            <input ref={ref0} type="number" placeholder="ادخل رقم الاسبوع..." className="p-3 appearance-none bg-inherit focus:outline-white text-white font-bold rounded-md"/>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 w-11/12 mx-auto md:grid-cols-3 xl:grid-cols-4 mb-5">
-            <Card dayy={day[0]} days={days.filter((item) => item.dayName == day[0])}/>
-            <Card dayy={day[1]} days={days.filter((item) => item.dayName == day[1])}/>
-            <Card dayy={day[2]} days={days.filter((item) => item.dayName == day[2])}/>
-            <Card dayy={day[3]} days={days.filter((item) => item.dayName == day[3])}/>
-            <Card dayy={day[4]} days={days.filter((item) => item.dayName == day[4])}/>
-            <Card dayy={day[5]} days={days.filter((item) => item.dayName == day[5])}/>
-            <Card dayy={day[6]} days={days.filter((item) => item.dayName == day[6])}/>
-            <Card dayy={day[7]} days={days.filter((item) => item.dayName == day[7])}/>
+           {day.map((item) => {
+            return(
+                <Card dayy={item} days={days.filter((itemm) => itemm.dayName == item)}/>
+            )
+           })}
         </div>
         <div className="w-full flex justify-center">
-            <div onClick={saveWeek} className="p-2 bg-black text-white font-bold text-2xl rounded-md hover:bg-zinc-900 transti hover:p-3 cursor-pointer ml-5">حفظ الكل</div>
-            <Link href="/"><div onClick={deleteAll} ref={ref00} className="p-2 bg-black text-white font-bold text-2xl rounded-md hover:bg-zinc-900 transti hover:p-3 cursor-pointer">رجوع</div></Link>
+            <div onClick={saveWeek} className="p-2 bg-bgTaskes text-white font-bold text-2xl rounded-md hover:bg-bgNav transition-all hover:p-3 cursor-pointer ml-5">حفظ الكل</div>
+            <Link href="/"><div onClick={deleteAll} ref={ref00} className="p-2 bg-bgTaskes text-white font-bold text-2xl rounded-md hover:bg-bgNav transition-all hover:p-3 cursor-pointer">رجوع</div></Link>
         </div>
     </div>
   )
--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -29,17 +29,17 @@ export default function Card({dayy, days}) {
     
 
   return (
-    <div className="h-[370px] mt-12 p-3 rounded-3xl bg-zinc-900 ">
-                <p className="w-4/5 p-3 rounded-lg text-xl text-white font-bold mt-2 mb-2 mx-auto text-center bg-black">{dayy}</p>
-                <div className={`test py-3 px-4 bg-black text-white rounded-b-3xl h-64 overflow-y-auto`}>
+    <div className="h-[370px] mt-12 p-3 rounded-3xl bg-bgCard ">
+                <p className="w-4/5 p-3 rounded-lg text-xl text-white font-bold mt-2 mb-2 mx-auto text-center bg-bgNav">{dayy}</p>
+                <div className={`test py-3 px-4 bg-bgTaske text-white rounded-b-3xl h-64 overflow-y-auto`}>
                   <div className="flex  text-lg mt-2 mb-3 py-2 cursor-pointer">
-                    <input ref={ref1} type="text" placeholder="اضف مهمة..." className="w-4/5 h-full bg-zinc-900 text-white p-3 appearance-none "/>
-                    <div onClick={done} className="bg-zinc-900 min-w-8 mx-auto text-center text-white rounded-md hover:bg-zinc-700 transti cursor-pointer p-1 ">تم</div>
+                    <input ref={ref1} type="text" placeholder="اضف مهمة..." className="w-4/5 h-full rounded-md bg-bgTaskes text-white p-3 appearance-none focus:outline-white"/>
+                    <div onClick={done} className="bg-bgTaskes min-w-8 mx-auto text-center text-white rounded-md hover:bg-bgNav transti cursor-pointer p-1 ">تم</div>
                   </div>
                   {days[0] !== undefined? days[0].tasks.map((item, index) => {
           
                     return(
-                      <div key={index} className="flex justify-between items-center w-full transition-all bg-zinc-900 text-white p-2 mb-1 cursor-pointer hover:bg-zinc-950 rounded-sm">
+                      <div key={index} className="flex justify-between items-center w-full transition-all bg-bgTaskes text-white p-2 mb-1 cursor-pointer hover:bg-bgNav rounded-md">
                         <p>{item.task}</p>
                         <p onClick={() => {deleteOne(dayy, index); forceUpdate()}} className="text-2xl hover:text-red-700">X</p>
                       </div>
