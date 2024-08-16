8f0b1f35aac5c0fc723f4c01c4a331e367d79980
Ubada <bhassan.ghamsh2020@gmail.com>
2024-08-16 21:20:41 +0300

    merge store

--- src/app/store/data.js
+++ src/app/store/data.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import {persist} from 'zustand/middleware'
-
+import { produce } from "immer";
+import moment from 'moment';
 
 export const useDayStore = create(persist((set) => ({
     day: [{dayName: "السبت", tasks: []}, {dayName: "الاحد", tasks: []},{dayName: "الاثنين", tasks: []}, {dayName: "الثلاثاء", tasks: []}, {dayName: "الاربعاء", tasks: []}, {dayName: "الخميس", tasks: []}, {dayName: "الجمعة", tasks: []}, {dayName: "تحديات الاسبوع", tasks: []}],
@@ -68,7 +69,32 @@ export const useDayStore = create(persist((set) => ({
 
         return ({weeks: all})
       }),
-   
+        // abdo
+        // data: [ {id: 0,flip: false,day: "السبت", tasks: [{id: 0,task: "one", state: false}, {id: 1,task: "two in sat", state: false}]},
+        //       {id: 1,flip: false,day: "الأحد", tasks: [{id: 0,task: "two", state: false}]},
+        //       {id: 2,flip: false,day: "الإثنين", tasks: [{id: 0,task: "three", state: false}]},
+        //       {id: 3,flip: false,day: "الثلاثاء", tasks: [{id: 0,task: "four", state: false}]},
+        //       {id: 4,flip: false,day: "الأربعاء", tasks:[{id: 0,task: "five", state: false}]},
+        //       {id: 5,flip: false,day: "الخميس", tasks:[{id: 0,task: "six", state: false}]},
+        //       {id: 6,flip: false,day: "الجمعة", tasks: [{id: 0,task: "seven", state: false}]},
+        //       {id: 7,flip: false,day: "تحديات الأسبوع", tasks: [{id: 0,task: "chang", state: false}]}],
+        data: [],
+        thisWeek: () => set(
+            (state) => {
+                const arr = state.weeks.filter((item) => item.numberWeek === state.numWeek())
+                return(console.log(arr))
+            }
+        ),
+        stateTask: (dayId, taskId) => set(
+        produce((draft) => {
+            draft.data[dayId].tasks[taskId].state = !draft.data[dayId].tasks[taskId].state
+        })
+      ),
+      stateFlip: (dayId) => set(
+        produce((draft) => {
+          draft.data[dayId].flip = !draft.data[dayId].flip
+        })
+      )
   }), {
       name: "week"
   }))
